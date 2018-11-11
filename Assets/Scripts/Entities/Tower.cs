d1e17a10f157e23b504a8d6a19da8858fcb2ff8a
ElasticSea <info@elasticsea.com>
2018-11-11 20:53:55 +0100

    Attack closest creep.

--- Assets/Scripts/Entities/Tower.cs
+++ Assets/Scripts/Entities/Tower.cs
@@ -10,16 +10,26 @@ namespace Assets.Scripts.Entities
         [SerializeField] private Transform attackPosition;
         [SerializeField] private float range;
         [SerializeField] private float interval;
-        [SerializeField] private TowerUpgrade[] Upgrades;
+        [SerializeField] private TowerUpgrade[] upgrades;
 
         private float lastAttack;
+
+        public TowerUpgrade[] Upgrades => upgrades;
 
         private void Update()
         {
             if (lastAttack + interval > Time.time) return;
 
             var creeps = FindObjectsOfType<Creep>();
-            var target  = creeps.FirstOrDefault(c => c.transform.position.Distance(transform.position) <= range);
+            var target = creeps
+                .Select(c =>
+                {
+                    var distance = c.transform.position.Distance(transform.position);
+                    var foo = new {Creep = c, Distance = distance};
+                    return foo;
+                })
+                .OrderBy(pair => pair.Distance)
+                .FirstOrDefault(pair => pair.Distance <= range)?.Creep;
 
             if (target)
             {
